f9b2be416d8c56b2cdca16a18d6eb89c69150725
omarfaruk-1 <farukomar.eg@gmail.com>
2026-08-29 02:08:44 +0600

    add frontend production url

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ import cors from "cors";
 const app  = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://vercel.com/omar-faruk2/findback",
   credentials: true,
 }));
 
